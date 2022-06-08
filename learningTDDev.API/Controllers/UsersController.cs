65b5845a47df465860392a49d9ea03b1c3971767
Spha Zolakhe <siphamandla@coralite.co.za>
2022-06-08 16:18:46 +0200

    Add project files.

---
+++ learningTDDev.API/Controllers/UsersController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace learningTDDev.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class UsersController : ControllerBase
+{
+   
+    private readonly ILogger<UsersController> _logger;
+    public UsersController(ILogger<UsersController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetUsers")]
+    public async Task<IActionResult> Get()
+    {
+        return null;
+    }
+}
---
+++ learningTDDev.UnitTests/Systems/Controllers/TestUsersController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace learningTDDev.UnitTests.Systems.Controllers
+{
+    public  class TestUsersController
+    {
+        [Fact]
+        public void GetUsers()
+        {
+
+        }
+    }
+}
